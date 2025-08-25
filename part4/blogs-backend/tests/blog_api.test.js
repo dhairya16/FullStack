c40721e00a792783b58dddf9320c242be28bf86c
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2025-08-25 22:38:00 +0530

    part4 4.13-4.14

--- part4/blogs-backend/tests/blog_api.test.js
+++ part4/blogs-backend/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -71,6 +71,43 @@ test('if title or url is not present in blog then return status 400', async () =
 
   const response = await api.post('/api/blogs').send(newBlog)
   assert.strictEqual(response.status, 400)
+})
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    const titles = blogsAtEnd.map((b) => b.title)
+    assert(!titles.includes(blogToDelete.title))
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+  })
+})
+
+describe('updation of a blog', () => {
+  test('succeeds', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+
+    const updatedBlog = {
+      ...blogToUpdate,
+      likes: 100,
+    }
+
+    await api.put(`/api/blogs/${blogToUpdate.id}`).send(updatedBlog).expect(200)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    const blogAfterUpdate = blogsAtEnd.find(
+      (b) => b.title === blogToUpdate.title
+    )
+    assert.strictEqual(blogAfterUpdate.likes, 100)
+  })
 })
 
 after(async () => {
