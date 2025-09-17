df8f029401bcea8b23ce207b3a0ceab918580eb9
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2025-09-17 17:39:36 +0530

    part9 9.16

--- part9/ts-react/src/App.tsx
+++ part9/ts-react/src/App.tsx
@@ -1,17 +1,35 @@
-const courseParts = [
-  {
-    name: "Fundamentals",
-    exerciseCount: 10
-  },
-  {
-    name: "Using props to pass data",
-    exerciseCount: 7
-  },
-  {
-    name: "Deeper type usage",
-    exerciseCount: 14
-  }
-];
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+interface CoursePartDesc extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartBasic extends CoursePartBase, CoursePartDesc {
+  // description: string;
+  kind: "basic"
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+interface CoursePartBackground extends CoursePartBase, CoursePartDesc {
+  // description: string;
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+interface CoursePartSpecial extends CoursePartBase, CoursePartDesc {
+  requirements: string[]
+  kind: "special"
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartSpecial;
+
 
 interface HeaderProps {
   courseName: string
@@ -23,17 +41,52 @@ const Header = (props: HeaderProps) => {
   </>
 }
 
-const Content = () => {
+interface PartProps {
+  data: CoursePart;
+}
+
+const Part = ({ data }: PartProps) => {
+  switch (data.kind) {
+    case 'basic':
+      return (
+        <div>
+          <b>{data.name} {data.exerciseCount}</b>
+          <div>{data.description}</div>
+        </div>
+      )
+    case 'group':
+      return (
+        <div>
+          <b>{data.name} {data.exerciseCount}</b>
+          <div>project exercises {data.groupProjectCount}</div>
+        </div>
+      )
+    case 'background':
+      return (
+        <div>
+          <b>{data.name} {data.exerciseCount}</b>
+          <div>{data.description}</div>
+          <div>submit to {data.backgroundMaterial}</div>
+        </div>
+      )
+    case 'special':
+      return (
+        <div>
+          <b>{data.name} {data.exerciseCount}</b>
+          <div>{data.description}</div>
+          <div>required skills: {data.requirements.join(', ')}</div>
+        </div>
+      )
+  }
+}
+
+interface ContentProps {
+  courseParts: CoursePart[]
+}
+
+const Content = ({ courseParts }: ContentProps) => {
   return <>
-    <p>
-      {courseParts[0].name} {courseParts[0].exerciseCount}
-    </p>
-    <p>
-      {courseParts[1].name} {courseParts[1].exerciseCount}
-    </p>
-    <p>
-      {courseParts[2].name} {courseParts[2].exerciseCount}
-    </p>
+    {courseParts.map((cP, idx) => <Part key={idx} data={cP} />)}
   </>
 }
 
@@ -51,13 +104,53 @@ const Total = (props: TotalProps) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
+  const courseParts: CoursePart[] = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special"
+    }
+  ];
 
   const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
 
   return (
     <div>
       <Header courseName={courseName} />
-      <Content />
+      <Content courseParts={courseParts} />
       <Total totalExercises={totalExercises} />
     </div>
   );
